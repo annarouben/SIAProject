577b11bd1fd871d40d16e9d8bb05396f4575fffb
annarouben <annarouben@gmail.com>
2025-06-24 09:54:02 -0700

    Updated Task card goodATTask buttons, added hover tip, truncated at certain char

--- src/components/icons/NewtonLogo.jsx
+++
@@ -1,9 +0,0 @@
-const NewtonLogo = () => (
-  <img 
-    src={import.meta.env.BASE_URL + 'assets/img/newtonLogo.png'}
-    alt="Newton Logo"
-    className="h-8 w-8" 
-  />
-);
-
-export default NewtonLogo;
--- src/constants/taskConstants.js
+++
@@ -1,6 +0,0 @@
-export const TASK_STATUS = {
-  PENDING_REVIEW: 'Pending Review',
-  PENDING_APPROVAL: 'Pending Approval',
-  IN_PROGRESS: 'In Progress',
-  COMPLETED: 'Completed'
-};
