1ee32170af913a47b4de8c82b089b5a5ccf8cef9
annarouben <annarouben@gmail.com>
2025-06-27 16:22:41 -0700

    Updated vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,25 +2,29 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
-export default defineConfig({
-  plugins: [react()],
-  base: '',
-  build: {
-    outDir: 'dist'
-  },
-  server: {
-    port: 5173,
-    host: true,
-    strictPort: false,
-    hmr: {
-      overlay: true
+export default defineConfig(({ command }) => {
+  const base = command === 'serve' ? '' : '/SIAProject/'
+  
+  return {
+    plugins: [react()],
+    base: base,
+    build: {
+      outDir: 'dist'
     },
-    open: true,
-    cors: true
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src')
+    server: {
+      port: 5173,
+      host: true,
+      strictPort: false,
+      hmr: {
+        overlay: true
+      },
+      open: true,
+      cors: true
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src')
+      }
     }
   }
 })
